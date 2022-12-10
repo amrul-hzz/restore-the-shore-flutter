8673d90f3bf526b8a2cb19e6e85b1543d7e4a2ac
amanda.nurul11 <amanda.nurul11@ui.ac.id>
2022-12-10 21:26:14 +0700

    add authentication barrier

--- lib/forum/page/forum_page.dart
+++ lib/forum/page/forum_page.dart
@@ -1,7 +1,10 @@
 
 import 'package:flutter/material.dart';
-//import 'package:restore_the_shore_flutter/colorpalette.dart';
+import 'package:restore_the_shore_flutter/colorpalette.dart';
 import 'package:restore_the_shore_flutter/nav_bar.dart';
+import 'package:pbp_django_auth/pbp_django_auth.dart';
+import 'package:provider/provider.dart';
+//
 
 class ForumPage extends StatefulWidget {
   const ForumPage({super.key});
@@ -13,11 +16,15 @@ class ForumPage extends StatefulWidget {
 }
 
 class _ForumPageState extends State<ForumPage> {
+
   @override
   Widget build(BuildContext context) {
+    final request = context.watch<CookieRequest>();
+
     return Scaffold(
       bottomNavigationBar: const NavBar(), // ini cara buatr NavBar nya, jangan lupa import dulu
-      body: Center(
+      body: request.loggedIn
+      ? Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
@@ -27,8 +34,45 @@ class _ForumPageState extends State<ForumPage> {
           ],
         ),
       ),
+
+      : Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.bottomCenter,
+            end: Alignment.topCenter,
+            colors: [
+              const Color.fromRGBO(93, 192, 211, 1.0),
+              ColorPalette.secondaryColor.shade800
+            ]
+          ),
+        ),
+
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const Text(
+                'You need to Login',
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+
+              const SizedBox(
+                height: 20,
+              ),
+
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.pushNamed(context, 'login');
+                },
+                child: const Text("Login"),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
-
-
 }
