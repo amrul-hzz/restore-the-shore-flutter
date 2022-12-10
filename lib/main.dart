1ab9e8327ad6a1416bf415c2ead6e1e1c49c175c
amanda.nurul11 <amanda.nurul11@ui.ac.id>
2022-12-10 22:29:23 +0700

    update packages

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,7 @@ import 'package:restore_the_shore_flutter/colorpalette.dart';
 // import 'package:restore_the_shore_flutter/leaderboard/leaderboard.dart';
 // import 'package:restore_the_shore_flutter/myAccount/page/myAccount.dart';
 // import 'package:restore_the_shore_flutter/create-event/create_event.dart';
-// import 'package:restore_the_shore_flutter/myAccount/page/login.dart';
+import 'package:restore_the_shore_flutter/myAccount/page/login.dart';
 import 'package:restore_the_shore_flutter/nav_bar.dart';
 import 'package:restore_the_shore_flutter/forum/page/forum_page.dart';
 import 'package:pbp_django_auth/pbp_django_auth.dart';
@@ -33,7 +33,7 @@ class MyApp extends StatelessWidget {
           // 'create-event': (context) => const CreateEventPage(),
           // 'leaderboard' : (context) => const LeaderboardPage(), // Nanti ini ganti aja Page nya
           // 'my-account' : (context) => const MyAccountPage(),
-          // 'login': (context) => const LoginPage(),
+          'login': (context) => const LoginPage(),
           // Nanti tambahin masing masing page nya ke sini
         },
         theme: ThemeData(
